38e01a1e2ee31f6d16cd1d5f24b0be2484de2be6
Ale Rodríguez <Ale.Rodriguez@hansencx.com>
2025-10-14 19:01:35 -0300

    Update urls.py to use CODESPACE_NAME environment variable and create CustomDefaultRouter for proper API root URL generation in Codespaces

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+import os
 from .views import (
     api_root,
     UserViewSet,
@@ -25,7 +26,52 @@ from .views import (
     WorkoutViewSet
 )
 
-router = DefaultRouter()
+# Get Codespace name from environment variable
+CODESPACE_NAME = os.getenv('CODESPACE_NAME')
+
+# Create router with custom root URL if in Codespaces
+class CustomDefaultRouter(DefaultRouter):
+    def get_api_root_view(self, api_urls=None):
+        api_root_dict = {}
+        list_name = self.routes[0].name
+        for prefix, viewset, basename in self.registry:
+            api_root_dict[prefix] = list_name.format(basename=basename)
+        
+        from rest_framework.views import APIView
+        from rest_framework.response import Response
+        from rest_framework.reverse import reverse
+        
+        class APIRoot(APIView):
+            def get(self, request, *args, **kwargs):
+                ret = {}
+                namespace = request.resolver_match.namespace
+                for key, url_name in api_root_dict.items():
+                    if namespace:
+                        url_name = namespace + ':' + url_name
+                    try:
+                        ret[key] = reverse(
+                            url_name,
+                            args=args,
+                            kwargs=kwargs,
+                            request=request,
+                            format=kwargs.get('format')
+                        )
+                    except:
+                        ret[key] = None
+                
+                # Update URLs to use CODESPACE_NAME if available
+                if CODESPACE_NAME:
+                    for key in ret:
+                        if ret[key]:
+                            ret[key] = ret[key].replace('http://', 'https://')
+                            if 'localhost' in ret[key] or '127.0.0.1' in ret[key]:
+                                ret[key] = f'https://{CODESPACE_NAME}-8000.app.github.dev/api/{key}/'
+                
+                return Response(ret)
+        
+        return APIRoot.as_view()
+
+router = CustomDefaultRouter()
 router.register(r'users', UserViewSet, basename='user')
 router.register(r'teams', TeamViewSet, basename='team')
 router.register(r'activities', ActivityViewSet, basename='activity')
